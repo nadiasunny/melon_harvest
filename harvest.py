7ee2b6b3cb209b2a2ee228e239db064403eaeccf
Nadia Hina <nadianadia062@gmail.com>
2023-12-06 12:07:57 -0800

    initalized attributes for melon and allowed for adding pairings

--- harvest.py
+++ harvest.py
@@ -10,19 +10,25 @@ class MelonType:
         self, code, first_harvest, color, is_seedless, is_bestseller, name
     ):
         """Initialize a melon."""
-
+        self.code = code
+        self.first_harvest = first_harvest
+        self.color = color
+        self.is_seedless = is_seedless
+        self.is_bestseller = is_bestseller 
+        self.name = name
+        
         self.pairings = []
 
         # Fill in the rest 
 
     def add_pairing(self, pairing):
         """Add a food pairing to the instance's pairings list."""
+        self.pairings = self.pairings.append(pairing)
 
-        # Fill in the rest
 
     def update_code(self, new_code):
         """Replace the reporting code with the new_code."""
-
+        self
         # Fill in the rest
 
 
